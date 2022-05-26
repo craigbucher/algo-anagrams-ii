e543f440d52cb30ed473c97f5837355d3df55f0c
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-05-25 21:23:04 -0500

    initial python

--- python/anagram2.py
+++ python/anagram2.py
@@ -1,4 +1,16 @@
-# Don't forget to run the tests (and create some of your own)
-
 def anagrams_for(word, list_of_words):
-		# your code here
+	result = []
+	string1 = word.upper()
+	arr1 = list(''.join(string1.split(' ')))
+	arr1.sort()
+	for i in range(0, len(list_of_words)):
+		string2 = list_of_words[i].lower()
+		arr2 = list(''.join(string2.split(' ')))
+		arr2.sort()
+		if (len(arr1) != len(arr2)):
+			continue
+		for j in range(0, len(arr1)):
+			if (arr1[j] != arr2[j]):
+				continue
+		result.append(list_of_words[i])
+	return(result)
