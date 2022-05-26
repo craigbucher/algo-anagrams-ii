132d69f3ad71007f4a8d0e460b426a12f512799f
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-05-25 21:04:59 -0500

    working js

--- js/anagram2.js
+++ js/anagram2.js
@@ -1,3 +1,20 @@
 exports.anagramsFor = function(word, listOfWords) {
-
+    const result = [];
+    string1 = word.toLowerCase();
+    let arr1 = string1.split(' ').join('').split('').sort();
+    for (let i = 0; i < listOfWords.length; i++){
+        let string2 = listOfWords[i].toLowerCase();
+        let arr2 = string2.split(' ').join('').split('').sort(); 
+        if ((arr1.length !== arr2.length)){
+            continue;
+        }
+        for (let j = 0; j < arr1.length; j++){	
+            if (arr1[j] !== arr2[j]){	
+              continue;	
+            }	
+        }
+        result.push(listOfWords[i]);
+    }
+    return result;
 };
+console.log(exports.anagramsFor('threads', ["threads", "trashed", "hardest", "hatreds", "hounds"]))
